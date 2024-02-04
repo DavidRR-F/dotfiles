76744c729a44c267f2e2d5b269ca710d13cdde20
DavidRR-F <david.rosefranklin96@gmail.com>
2024-02-04 17:49:13 -0500

    open file tree init

--- nvim/lua/david/plugins/nvim-tree.lua
+++ nvim/lua/david/plugins/nvim-tree.lua
@@ -1,3 +1,11 @@
+function OpenFileInNewTab()
+  local lib = require'nvim-tree.lib'
+  local node = lib.get_node_at_cursor()
+  if node then
+      vim.cmd('tabnew ' .. node.absolute_path)
+  end
+end
+
 return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -6,49 +14,87 @@ return {
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
         vim.opt.termguicolors = true
+        vim.api.nvim_exec([[
+        autocmd VimEnter * NvimTreeOpen
+        autocmd VimEnter * wincmd p
+        ]], false)
         vim.cmd([[ highlight NvimTreeFolderArrowClosed guifg=#3FC5FF ]])
         vim.cmd([[ highlight NvimTreeFolderArrowOpen guifg=#3FC5FF ]])
         local nvimtree = require("nvim-tree")
         nvimtree.setup({
-            view = {
-              width = 35,
-              relativenumber = true,
+          disable_netrw = true,
+          hijack_netrw = true,
+          hijack_cursor = true,
+          hijack_unnamed_buffer_when_opening = false,
+          sync_root_with_cwd = true,
+          update_focused_file = {
+            enable = true,
+            update_root = false,
+          },
+          view = {
+            adaptive_size = false,
+            side = "left",
+            width = 30,
+            preserve_window_proportions = true,
+          },
+          git = {
+            enable = false,
+            ignore = true,
+          },
+          filesystem_watchers = {
+            enable = true,
+          },
+          actions = {
+            open_file = {
+              resize_window = true,
             },
-            -- change folder arrow icons
-            renderer = {
-              indent_markers = {
-                enable = true,
+          },
+          renderer = {
+            root_folder_label = false,
+            highlight_git = false,
+            highlight_opened_files = "none",
+        
+            indent_markers = {
+              enable = false,
+            },
+        
+            icons = {
+              show = {
+                file = true,
+                folder = true,
+                folder_arrow = true,
+                git = false,
               },
-              icons = {
-                glyphs = {
-                  folder = {
-                    arrow_closed = "", -- arrow when folder is closed
-                    arrow_open = "", -- arrow when folder is open
-                  },
+        
+              glyphs = {
+                default = "󰈚",
+                symlink = "",
+                folder = {
+                  default = "",
+                  empty = "",
+                  empty_open = "",
+                  open = "",
+                  symlink = "",
+                  symlink_open = "",
+                  arrow_open = "",
+                  arrow_closed = "",
+                },
+                git = {
+                  unstaged = "✗",
+                  staged = "✓",
+                  unmerged = "",
+                  renamed = "➜",
+                  untracked = "★",
+                  deleted = "",
+                  ignored = "◌",
                 },
               },
             },
-            -- disable window_picker for
-            -- explorer to work well with
-            -- window splits
-            actions = {
-              open_file = {
-                window_picker = {
-                  enable = false,
-                },
-              },
-            },
-            filters = {
-              custom = { ".DS_Store" },
-            },
-            git = {
-              ignore = false,
-            },
+          },
           })
       
           -- set keymaps
           local keymap = vim.keymap -- for conciseness
-      
           keymap.set('n', '<C-n>', "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
         end,
 }
