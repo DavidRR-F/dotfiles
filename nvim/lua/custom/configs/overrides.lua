d6ecd97fe4fbffb0924f3c4184d7f20ca132071d
David Rose-Franklin <david.rosefranklin@maryvillecollege.edu>
2024-02-06 16:49:07 -0500

    golang config

--- nvim/lua/custom/configs/overrides.lua
+++ nvim/lua/custom/configs/overrides.lua
@@ -14,6 +14,7 @@ M.treesitter = {
     "json",
     "yaml",
     "bash",
+    "go",
     "markdown",
     "markdown_inline",
   },
@@ -40,6 +41,9 @@ M.mason = {
 
     -- python stuff
     "pyright",
+
+    -- golang stuff
+    "gopls",
   },
 }
 
