89f3e637e08348323d027e6bc42ac28bb230da15
DavidRR-F <david.rosefranklin96@gmail.com>
2024-04-11 20:07:53 -0400

    dbui update

--- nvim/lua/david/core/keymaps.lua
+++ nvim/lua/david/core/keymaps.lua
@@ -14,4 +14,11 @@ keymap.set("n", "<ScrollWheelDown>", "<C-E>", { silent = true })
 
 -- tab indent
 keymap.set("v", "<Tab>", ">gv", { silent = true })
-keymap.set("v", "<S-Tab>", "<gv", { silent = true })
+keymap.set("v", "<S-Tab>", "<gv", { silent = true }) 
+
+-- resize window
+vim.api.nvim_set_keymap('n', '<Leader>ww', ':resize +5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>ws', ':resize -5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>wa', ':vertical resize +5<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>wd', ':vertical resize -5<CR>', { noremap = true, silent = true })
+
