00100d56aee560bdb3bc8638c42d59bd52cedd76
DavidRR-F <david.rosefranklin96@gmail.com>
2024-10-16 19:22:28 -0400

    yazi plugin

--- terminal/wezterm/lua/font.lua
+++ terminal/wezterm/lua/font.lua
@@ -4,7 +4,7 @@ local M = {}
 -- The Font rules are being applied here.
 M.apply_to_config = function(c)
   c.font = wez.font { family = "0xProto Nerd Font Mono", weight = "Regular" }
-  c.font_size = 16
+  c.font_size = 18
   c.font_rules = {
     {
       intensity = "Normal",
--- tui/nvim/lua/david/plugins/gui/noice.lua
+++ tui/nvim/lua/david/plugins/gui/noice.lua
@@ -10,31 +10,14 @@ return {
   },
   config = function()
     require("noice").setup({
-      lsp = {
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
       presets = {
         bottom_search = true,
         command_palette = true,
         long_message_to_split = false,
         inc_rename = true,
-        lsp_doc_border = true,
+        lsp_doc_border = false,
       },
       views = {
-        cmdline_popup = {
-          position = {
-            row = 5,
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = "auto",
-          },
-        },
         popupmenu = {
           relative = "editor",
           position = {
@@ -69,14 +52,6 @@ return {
           filter = {
             event = "msg_show",
             kind = "",
-            find = "utils.lua",
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            event = "msg_show",
-            kind = "",
             find = "written",
           },
           opts = { skip = true },
@@ -107,24 +82,19 @@ return {
       },
       cmdline = {
         enabled = true,
-        view = "cmdline_popup",
+        view = "cmdline",
         format = {
           opts = {},
           cmdline = {
             pattern = "^:",
             icon = " ",
-            lang = "vim",
-            opts = {
-              position = "50%",
-              row = "50%",
-              border = "rounded",
-            },
+            lang = "vim"
           },
-          search_down = { kind = "search", pattern = "^/", icon = " ", lang = "regex" },
-          search_up = { kind = "search", pattern = "^%?", icon = " ", lang = "regex" },
-          filter = { pattern = "^:%s*!", icon = "$", lang = "bash" },
-          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = "", lang = "lua" },
-          help = { pattern = "^:%s*he?l?p?%s+", icon = "" },
+          search_down = { kind = "search", pattern = "^/", icon = "  ", lang = "regex" },
+          search_up = { kind = "search", pattern = "^%?", icon = "  ", lang = "regex" },
+          filter = { pattern = "^:%s*!", icon = " $", lang = "bash" },
+          lua = { pattern = { "^:%s*lua%s+", "^:%s*lua%s*=%s*", "^:%s*=%s*" }, icon = " ", lang = "lua" },
+          help = { pattern = "^:%s*he?l?p?%s+", icon = " " },
           input = {},
         },
       },
