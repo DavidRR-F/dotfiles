16ab7df1a3b6cad36ec6e11110b1e2e7eac96f7f
DavidRR-F <david.rosefranklin96@gmail.com>
2024-10-16 21:16:39 -0400

    dashboard

---
+++ tui/nvim/lua/david/plugins/gui/dashboard.lua
@@ -0,0 +1,73 @@
+return {
+  'nvimdev/dashboard-nvim',
+  event = 'VimEnter',
+  config = function()
+    require('dashboard').setup {
+      theme = 'doom',
+      config = {
+        header = {
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[  ____                _ _____ _          ____                  ]],
+          [[ |  _ \ ___  __ _  __| |_   _| |__   ___|  _ \  ___   ___ ___  ]],
+          [[ | |_) / _ \/ _` |/ _` | | | | '_ \ / _ \ | | |/ _ \ / __/ __| ]],
+          [[ |  _ <  __/ (_| | (_| | | | | | | |  __/ |_| | (_) | (__\__ \ ]],
+          [[ |_| \_\___|\__,_|\__,_| |_| |_| |_|\___|____/ \___/ \___|___/ ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+          [[                                                               ]],
+        },
+        center = {
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find File',
+            desc_hl = 'String',
+            key = 'f',
+            keymap = 'leader ff',
+            key_hl = 'Number',
+            key_format = ' %s', 
+            action = function() require('telescope.builtin').find_files() end
+          },
+          {
+            icon = ' ',
+            icon_hl = 'Title',
+            desc = 'Find Worktree',
+            desc_hl = 'String',
+            key = 'w',
+            keymap = 'leader fw',
+            key_format = ' %s', 
+            action = function() require('telescope').extensions.git_worktree.git_worktrees() end
+          },
+          {
+            icon = '󰷊 ',
+            icon_hl = 'Title',
+            desc = 'File Explorer',
+            desc_hl = 'String',
+            key = 'e',
+            keymap = 'leader fe',
+            key_format = ' %s', 
+            action = function() vim.cmd('Yazi cwd') end
+          },
+          {
+            icon = '󰈆 ',
+            icon_hl = 'Title',
+            desc = 'Quit',
+            desc_hl = 'String',
+            key = 'q',
+            keymap = ':q',
+            key_format = ' %s', 
+            action = function() vim.cmd('qa') end
+          },
+        },
+        footer = {},
+      }
+    }
+  end,
+  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}
--- tui/nvim/lua/david/plugins/gui/nav/yazi.lua
+++ tui/nvim/lua/david/plugins/gui/nav/yazi.lua
@@ -1,6 +1,13 @@
 return {
   "mikavilpas/yazi.nvim",
   event = "VeryLazy",
+  keys = {
+    {
+      "<leader>fe",
+      "<cmd>Yazi cwd<cr>",
+      desc = "Open the file manager in nvim's working directory" ,
+    },
+  },
   opts = {
     open_for_directories = true,
     keymaps = {
