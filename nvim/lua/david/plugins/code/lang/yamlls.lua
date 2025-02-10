e7760dcea3787e68d99035bf751be969916db740
DavidRR-F <david.rosefranklin96@gmail.com>
2025-02-09 19:37:55 -0500

    dynamically add yaml schemas per buffer

--- nvim/lua/david/plugins/code/lang/yamlls.lua
+++ nvim/lua/david/plugins/code/lang/yamlls.lua
@@ -7,31 +7,12 @@ return {
       },
       hover = true,
       validate = true,
-      schemas = {
-        -- Kubernetes Schema
-        ["https://raw.githubusercontent.com/yannh/kubernetes-json-schema/master/v1.27.3-standalone-strict/all.json"] = {
-          "configmap.{yml,yaml}",
-          "deployment.{yml,yaml}",
-          "service.{yml,yaml}",
-          "ingress.{yml,yaml}",
-          "secret.{yml,yaml}",
-        },
-        -- Kustomize Schema
-        ["https://json.schemastore.org/kustomization.json"] = {
-          "kustomization.{yml,yaml}"
-        },
-        -- Gitlab Schema
-        ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
-          ".gitlab-ci.{yml,yaml}",
-          "*-common.{yml,yaml}",
-          "*-template.{yml,yaml}"
-        },
-      },
+      schemas = {},
       schemaStore = {
         enable = false
       },
     }
   },
-  filetypes = { "yaml" },
-  single_file_support = true
+  filetypes = { "yaml", "yml" },
+  single_file_support = true,
 }
--- nvim/lua/david/plugins/code/lsp.lua
+++ nvim/lua/david/plugins/code/lsp.lua
@@ -7,6 +7,27 @@ local servers = {
   "jsonls",
 }
 
+local yaml_schema_mappings = {
+  -- Kubernetes Schema
+  kubernetes = {
+    "configmap",
+    "deployment",
+    "service",
+    "ingress",
+    "secret",
+  },
+  -- Kustomize Schema
+  ["https://json.schemastore.org/kustomization.json"] = {
+    "kustomization",
+  },
+  -- GitLab Schema
+  ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = {
+    ".gitlab-ci",
+    "-common",
+    "-template",
+  },
+}
+
 return {
   {
     "williamboman/mason.nvim",
@@ -36,13 +57,34 @@ return {
       local lspconfig = require("lspconfig")
       local keymap = vim.keymap
       local opts = { noremap = true, silent = true }
-      local on_attach = function(_, bufnr)
+      local on_attach = function(client, bufnr)
         opts.buffer = bufnr
         keymap.set('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<CR>', opts)
         keymap.set('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<CR>', opts)
         keymap.set('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<CR>', opts)
         keymap.set('v', 'gr', '<Cmd>lua vim.lsp.buf.rename()<CR>', opts)
         keymap.set('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        if vim.bo.filetype == "yaml" or vim.bo.filetype == "yml" then
+          local bufname = vim.api.nvim_buf_get_name(bufnr)
+          local filename = vim.fn.fnamemodify(bufname, ":t") -- Get just the filename
+          -- Find the matching schema
+          local selected_schema = nil
+          for schema_url, patterns in pairs(yaml_schema_mappings) do
+            for _, pattern in ipairs(patterns) do
+              if filename:match("^" .. pattern) then
+                selected_schema = schema_url
+                break
+              end
+            end
+            if selected_schema then break end
+          end
+
+          -- Apply the schema dynamically
+          if selected_schema then
+            client.config.settings.yaml.schemas = { [selected_schema] = "*" }
+          end
+
+        end
       end
       for _, server in pairs(servers) do
         local lsp_opts = { on_attach = on_attach }
