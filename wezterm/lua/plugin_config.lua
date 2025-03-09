ea237d6e498e5ccc74e588a9515bd5ec1e9bd111
DavidRR-F <david.rosefranklin96@gmail.com>
2025-03-09 19:05:46 -0400

    color variables

--- wezterm/lua/plugin_config.lua
+++ wezterm/lua/plugin_config.lua
@@ -3,8 +3,8 @@ local scheme = wez.color.get_builtin_schemes()["Catppuccin Mocha"]
 
 local M = {}
 
-M.tab_background = 'rgb(24, 24, 37)'
-M.catpuccin_purple = '#cba6f7'
+M.tab_background = '{{ black }}'
+M.catpuccin_purple = '{{ magenta }}'
 
 local mode_icons = {
   NO = '', -- Normal Mode
