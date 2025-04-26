f3e705fc5d32a1bbda25300164e9665216479c66
DavidRR-F <david.rosefranklin96@gmail.com>
2025-04-26 18:33:03 -0400

    fix k9s screen dump

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -6,4 +6,9 @@ if [ "$(uname)" = "Linux" ]; then
   sudo chmod a+x ~/.local/bin/toggle-show-floating.sh
   sudo chmod a+x ~/.local/bin/floating-position.sh
   sudo chmod a+x ~/.config/polybar/modules/bluetooth.sh
+  envsubst <~/.config/k9s/config.yaml >tmp.yaml
+  mv tmp.yaml ~/.config/k9s/config.yaml
+else # assume macos
+  envsubst <~/Library/Application Support/k9s/config.yaml >tmp.yaml
+  mv tmp.yaml ~/Library/Application Support/k9s/config.yaml
 fi
