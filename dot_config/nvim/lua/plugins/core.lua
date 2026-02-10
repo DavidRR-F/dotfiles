89f1e8d0c8d1c0a0f73afee0db3a1358178fa3b5
DavidRR-F <david.rosefranklin96@gmail.com>
2026-02-10 15:48:52 -0500

    Update .config/nvim/lua/plugins/core.lua Update .zsh/.zsh_env Update .zsh/.zsh_functions

--- dot_config/nvim/lua/plugins/core.lua
+++ dot_config/nvim/lua/plugins/core.lua
@@ -97,7 +97,13 @@ return {
       },
     },
   },
-  { "alker0/chezmoi.vim" },
+  {
+    "alker0/chezmoi.vim",
+    lazy = false,
+    init = function()
+      vim.g["chezmoi#use_tmp_buffer"] = true
+    end
+  },
   {
     'xvzc/chezmoi.nvim',
     dependencies = { 'nvim-lua/plenary.nvim' },
